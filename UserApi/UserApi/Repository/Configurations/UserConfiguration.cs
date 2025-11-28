5b80b74cfcb0dd7992a81f5d05ca5dc826eb706e
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-11-28 11:45:03 -0300

    Fix bug of user not having an id for account

--- UserApi/UserApi/Repository/Configurations/UserConfiguration.cs
+++ UserApi/UserApi/Repository/Configurations/UserConfiguration.cs
@@ -11,6 +11,7 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.ToTable("Users");
         
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedNever();
 
         builder
             .Property(x => x.Email)
