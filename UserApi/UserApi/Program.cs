22cc04f1ada503fa380a09761334da6781a8f43f
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-11-29 15:12:14 -0300

    Add swagger gen and ui; Add jwt authentication

--- UserApi/UserApi/Program.cs
+++ UserApi/UserApi/Program.cs
@@ -1,23 +1,43 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using ServiceCommons.Jwt;
 using UserApi.Repository;
 using UserApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddAuthorization();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<UserDbContext>(options =>
 {
     options.UseNpgsql(connectionString);
 });
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UserApi",
+        Version = "v1",
+    });
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserApi v1");
+    });
+}
 
 app.MapControllers();
 app.Run();
