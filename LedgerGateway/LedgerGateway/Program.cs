3af4e9631da7b6c7f37e593297ba1914e021416c
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-01 20:59:39 -0300

    Improve program readability and configure clients

--- LedgerGateway/LedgerGateway/Program.cs
+++ LedgerGateway/LedgerGateway/Program.cs
@@ -1,4 +1,6 @@
+using LedgerGateway.RestClients;
 using Microsoft.OpenApi.Models;
+using ServiceCommons.ApiKey;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,11 +15,8 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddGrpcClient<FinancialService.FinancialService.FinancialServiceClient>(o =>
-{
-    o.Address = new Uri(builder.Configuration["FinancialService:GrpcUrl"]
-                        ?? throw new InvalidOperationException("Missing FinancialService:GrpcUrl"));
-});
+
+ConfigureClients(builder);
 
 var app = builder.Build();
 
@@ -27,5 +26,40 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "LedgerGateway v1"); });
 }
 
-
 app.Run();
+return;
+
+void ConfigureClients(WebApplicationBuilder webApplicationBuilder)
+{
+    var integration = builder.Configuration.GetSection("Integration");
+    
+    webApplicationBuilder.Services.AddTransient<ApiKeyHttpMesageHandler>(_ =>
+    {
+        var apiKey = GetOrThrow("ClientApi", "ApiKey");
+        return new ApiKeyHttpMesageHandler(apiKey);
+    });
+
+    webApplicationBuilder.Services.AddHttpClient<UserApiClient>(client =>
+    {
+        client.BaseAddress = new Uri(GetOrThrow("ClientApi", "BaseUrl"));
+    })
+    .AddHttpMessageHandler<ApiKeyHttpMesageHandler>();
+    
+    webApplicationBuilder.Services.AddTransient<ApiKeyGrpcInterceptor>(_ =>
+    {
+        var apiKey = GetOrThrow("FinancialService", "ApiKey");
+        return new ApiKeyGrpcInterceptor(apiKey);
+    });
+    
+    webApplicationBuilder.Services.AddGrpcClient<FinancialService.FinancialService.FinancialServiceClient>(o =>
+    {
+        o.Address =new Uri(GetOrThrow("FinancialService", "BaseUrl"));
+    }).AddInterceptor<ApiKeyGrpcInterceptor>();
+    return;
+
+    string GetOrThrow(string serviceName, string key)
+    {
+        return integration.GetSection(serviceName).GetValue<string>(key) 
+            ?? throw new InvalidOperationException($"{serviceName}.{key} not configured.");
+    }
+}
