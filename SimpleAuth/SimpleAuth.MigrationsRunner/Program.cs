6f8a7b34381d60c64125bb4be2c5de593f5e8b79
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-04 00:03:39 -0300

    Add migrations runner init container to user api and simple auth

---
+++ SimpleAuth/SimpleAuth.MigrationsRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleAuth.Api.Data;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+
+Console.WriteLine("Running migrations...");
+db.Database.Migrate();
+Console.WriteLine("Done");
---
+++ UserApi/UserApi.MigrationsRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UserApi.Repository;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+
+Console.WriteLine("Running migrations...");
+db.Database.Migrate();
+Console.WriteLine("Done");
