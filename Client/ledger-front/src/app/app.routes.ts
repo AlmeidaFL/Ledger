36e883b8d494ed87f5711c4b82c3bda6a5bb9dcf
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-06 17:32:28 -0300

    Add deposit and transfer components

--- Client/ledger-front/src/app/app.routes.ts
+++ Client/ledger-front/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
+import { MainLayout as MainLayoutComponent } from './layout/main-layout/main-layout';
+import { Home as HomeComponent } from './home/home/home';
+import { Deposit as DepositComponent } from './home/deposit/deposit';
+import { Transfer as TransferComponent } from './home/transfer/transfer';
 
 export const routes: Routes = [
     {
@@ -8,8 +12,20 @@ export const routes: Routes = [
         children: [
             { path: 'login', component: LoginComponent },
             { path: 'register', component: RegisterComponent },
-            { path: '', redirectTo: 'login', pathMatch: 'full' }
+            // { path: '', redirectTo: 'login', pathMatch: 'full' }
         ]
     },
-    { path: '', redirectTo: 'auth/login', pathMatch: 'full'}
+    // { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: 'home', component: HomeComponent},
+            { path: 'home/deposit', component: DepositComponent},
+            { path: 'home/transfer', component: TransferComponent},
+            { path: '', redirectTo: 'home', pathMatch: 'full' }
+        ]
+    }
 ];
