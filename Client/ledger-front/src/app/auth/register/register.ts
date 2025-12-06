497ed7524d36bae554f4c7802df3efc20bdb1db5
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-06 16:39:57 -0300

    Create register component

--- Client/ledger-front/src/app/auth/register/register.ts
+++ Client/ledger-front/src/app/auth/register/register.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class RegisterComponent {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(12)]],
+      confirmPassword: ['', [Validators.required]]
+    })
+  }
+
+  submit() {
+    if (this.form.invalid){
+      return;
+    }
+
+    console.log("Sending data", this.form.value)
+  }
 }
