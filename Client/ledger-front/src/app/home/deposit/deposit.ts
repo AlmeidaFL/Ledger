d3d14b4303b2089e589630d72729368eb3fd879f
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-06 18:05:19 -0300

    Create deposit screen

--- Client/ledger-front/src/app/home/deposit/deposit.ts
+++ Client/ledger-front/src/app/home/deposit/deposit.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-deposit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './deposit.html',
   styleUrl: './deposit.css',
 })
 export class Deposit {
+  form: FormGroup
+  submittedValueInCents: number | null = null;
 
+  constructor(private formBuilder: FormBuilder){
+    this.form = this.formBuilder.group({
+      amount: ['', [Validators.required]]
+    })
+  }
+
+  submit() {
+    if (this.form.invalid){
+      alert('Form invalid. Please revise')
+      return;
+    }
+
+    const rawAmount = this.form.value.amount!
+    const normalizedAmount = rawAmount.replace(',', '.')
+
+    const floatAmount = parseFloat(normalizedAmount)
+
+    if (isNaN(floatAmount) || floatAmount <= 0){
+      alert('Invalid amount')
+      return;
+    }
+
+    const cents = Math.round(floatAmount * 100)
+    this.submittedValueInCents = cents;
+
+    console.log("Deposit in cents: ", cents)
+  }
 }
