165dc309dfa5f46472d596f9e10b229e978dad57
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-06 17:00:11 -0300

    Add login page

--- Client/ledger-front/src/app/auth/login/login.ts
+++ Client/ledger-front/src/app/auth/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log('Mock login:', this.form.value);
+  }
 }
