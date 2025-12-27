3dd78617234d9ba3a55643fec3936ae368ce6ec2
AlmeidaFL <ra00303863@pucsp.edu.br>
2025-12-27 20:02:26 -0300

    Ignore code build commits to avoid loop

--- aws/terraform/ecr.tf
+++ aws/terraform/ecr.tf
@@ -17,3 +17,8 @@
 #   name                 = "financial-service"
 #   image_tag_mutability = "MUTABLE"
 # }
+
+# resource "aws_ecr_repository" "user-api" {
+#   name                 = "user-api"
+#   image_tag_mutability = "MUTABLE"
+# }
