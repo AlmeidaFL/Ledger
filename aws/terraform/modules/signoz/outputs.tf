dd5bb8c408e46888b5e325ad4a47647d143f0c14
AlmeidaFL <ra00303863@pucsp.edu.br>
2026-01-02 18:20:40 -0300

    Transfer signoz from terraform deploy to argocd deploy

--- aws/terraform/modules/signoz/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "helm_release_name" {
-  description = "Name of the Helm release"
-  value       = helm_release.signoz.name
-}
-
-output "namespace" {
-  description = "Namespace where SigNoz is deployed"
-  value       = var.namespace
-}
